f0e8582bdf05d6d59465bdc421cecc4fc9bf8d39
maahir-cyber <maahirrajvinayak@gmail.com>
2026-08-04 11:00:29 +0530

    Configure firebase.js with correct keys and db export

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA1yfHn5CiqMzjunCQ-y6VjPEkWmk5Bs00",
   authDomain: "edushareconnect-cef72.firebaseapp.com",
@@ -19,3 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize and export Firestore database for cross-device syncing
+export const db = getFirestore(app);
